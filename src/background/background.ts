3519a6cefd92c739ca3eb58be7f9800368f46e4e
Sam Hamburger <samhamburger@MacBook-Pro-2.lan1>
2020-07-20 22:50:01 -0400

    data flow complete

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,2 +1,24 @@
 const isThisBackgroundScript = true;
 console.log('isThisBackgroundScript', isThisBackgroundScript);
+
+// chrome.runtime.onMessage.addListener((msg, sender) => {
+//   // First, validate the message's structure.
+//   if (msg.from === 'content' && msg.subject === 'showPageAction') {
+//     // Enable the page-action for the requesting tab.
+//     console.log('hi');
+//     console.log(sender.tab);
+//     chrome.pageAction.show(sender.tab.id);
+//   }
+// });
+
+// chrome.runtime.onInstalled.addListener(function () {
+//   chrome.storage.sync.set({ color: '#3aa757' }, function () {
+//     console.log('The color is green.');
+//   });
+// });
+
+// chrome.browserAction.onClicked.addListener(function (tab) {
+//   chrome.tabs.executeScript(null, {
+//     code: "alert(document.querySelector('p').innerText)",
+//   });
+// });
--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -1,4 +1,66 @@
 import './contentscript.scss';
+import { title } from 'process';
 
 const isThisContentscript = true;
 console.log('isThisContentscript', isThisContentscript);
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.command === 'test') {
+    const name = (<HTMLElement>(
+      document.querySelector('.pv-top-card--list').querySelector('.inline.t-24')
+    ))?.innerText;
+
+    const titleCompanyLine = (<HTMLElement>(
+      document.querySelector('.mt1.t-18.t-black.t-normal.break-words')
+    ))?.innerText;
+
+    const educationList = <NodeListOf<HTMLElement>>(
+      document.querySelectorAll('.pv-entity__school-name.t-16')
+    );
+
+    const yearsAtCurrent = (<HTMLElement>(
+      document.querySelector(
+        'div.pv-entity__company-summary-info > h4 > span:nth-child(2)',
+      )
+    ))?.innerText;
+
+    let licensesAndCerts = [];
+
+    document
+      .querySelectorAll(
+        'div.pv-certifications__summary-info.pv-entity__summary-info.pv-entity__summary-info--background-section.pv-certifications__summary-info--has-extra-details',
+      )
+      .forEach((certNode) =>
+        licensesAndCerts.push({
+          title: (<HTMLElement>certNode.querySelector('h3'))?.innerText,
+          issuedBy: (<HTMLElement>(
+            certNode.querySelector('p > span:nth-child(2)')
+          ))?.innerText,
+        }),
+      );
+
+    const location = (<HTMLElement>(
+      document.querySelector(
+        'ul.pv-top-card--list.pv-top-card--list-bullet.mt1 > li.t-16',
+      )
+    ))?.innerText;
+
+    const contactInfoButton = <HTMLElement>(
+      document.querySelector(
+        'ul.pv-top-card--list.pv-top-card--list-bullet.mt1 > li:nth-child(3) > a',
+      )
+    );
+
+    sendResponse({
+      name,
+      currentTitle: titleCompanyLine.split(' at ')[0],
+      currentCompany: titleCompanyLine.split(' at ')[1],
+      primaryEducation: educationList[0]?.innerText,
+      secondaryEducation: educationList[1]?.innerText,
+      yearsAtCurrent,
+      licensesAndCerts,
+      location,
+    });
+  }
+  /* Content script action */
+});
